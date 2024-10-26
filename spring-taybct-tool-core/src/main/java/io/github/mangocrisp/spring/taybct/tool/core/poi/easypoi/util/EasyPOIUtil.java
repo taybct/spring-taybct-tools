f701a5cbc43174ce36169cd5b7f6c66ed437c089
yinxijie <yinxijie@gzgxinfo.com>
2024-10-26 14:56:19 +0800

    - EasyPOI，通用代码 - Excel 可选字段导出

--- spring-taybct-tool-core/src/main/java/io/github/mangocrisp/spring/taybct/tool/core/poi/easypoi/util/EasyPOIUtil.java
+++ spring-taybct-tool-core/src/main/java/io/github/mangocrisp/spring/taybct/tool/core/poi/easypoi/util/EasyPOIUtil.java
@@ -5,17 +5,32 @@ import cn.afterturn.easypoi.excel.ExcelImportUtil;
 import cn.afterturn.easypoi.excel.entity.ExportParams;
 import cn.afterturn.easypoi.excel.entity.ImportParams;
 import cn.afterturn.easypoi.excel.entity.enmus.ExcelType;
+import cn.afterturn.easypoi.handler.inter.IExcelDictHandler;
+import cn.afterturn.easypoi.handler.inter.IWriter;
+import cn.afterturn.easypoi.word.WordExportUtil;
 import io.github.mangocrisp.spring.taybct.tool.core.util.StringUtil;
 import javax.servlet.http.HttpServletResponse;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import net.sf.jxls.transformer.XLSTransformer;
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.util.Assert;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
@@ -23,7 +38,112 @@ import java.util.NoSuchElementException;
 /**
  * EasyPoi 工具类
  */
+@Slf4j
 public class EasyPOIUtil {
+
+    public static IExcelDictHandler excelDictHandler;
+
+    /**
+     * 根据模板导出excel
+     * excel-template
+     *
+     * @param values   生成 Workbook 的属性
+     * @param tempName 模板路径
+     * @param dowName  文件下载名
+     * @param response 响应
+     * @throws IOException IO 异常
+     */
+    public static void exportExcelTemplate(List values, String tempName, String dowName, HttpServletResponse response) throws IOException {
+        //获得模版
+        ClassPathResource tempFileName = new ClassPathResource(tempName);
+        Map beans = new HashMap();
+        //导出列表名
+        beans.put("values", values);
+        XLSTransformer transformer = new XLSTransformer();
+        OutputStream out = response.getOutputStream();
+        try {
+            //将数据添加到模版中生成新的文件
+            Workbook workbook = transformer.transformXLS(tempFileName.getInputStream(), beans);
+            // 设置response参数，可以打开下载页面
+            String headStr = "attachment; filename=\"" + new String((dowName).getBytes("gb2312"), "ISO8859-1");
+            response.setContentType("octets/stream");
+            response.setContentType("APPLICATION/OCTET-STREAM");
+            response.setHeader("Content-Disposition", headStr);
+            workbook.write(out);
+        } catch (Exception e) {
+            throw new IOException(e.getMessage());
+        } finally {
+            try {
+                if (out != null)
+                    out.close();
+            } catch (IOException e) {
+                log.error("IO 关闭异常", e);
+            }
+        }
+    }
+
+    /**
+     * 导出word
+     * <p>第一步生成替换后的word文件，只支持docx</p>
+     * <p>第二步下载生成的文件</p>
+     * <p>第三步删除生成的临时文件</p>
+     * 模版变量中变量格式：{{foo}}
+     *
+     * @param templatePath word模板地址
+     * @param temDir       生成临时文件存放地址
+     * @param fileName     文件名
+     * @param params       替换的参数
+     * @param request      HttpServletRequest
+     * @param response     HttpServletResponse
+     */
+    public static void exportWord(String templatePath, String temDir, String fileName, Map<String, Object> params, HttpServletRequest request, HttpServletResponse response) {
+        Assert.notNull(templatePath, "模板路径不能为空");
+        Assert.notNull(temDir, "临时文件路径不能为空");
+        Assert.notNull(fileName, "导出文件名不能为空");
+        Assert.isTrue(fileName.endsWith(".docx"), "word导出请使用docx格式");
+        if (!temDir.endsWith("/")) {
+            temDir = temDir + File.separator;
+        }
+        File dir = new File(temDir);
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+        try {
+            String userAgent = request.getHeader("user-agent").toLowerCase();
+            if (userAgent.contains("msie") || userAgent.contains("like gecko")) {
+                fileName = URLEncoder.encode(fileName, StandardCharsets.UTF_8);
+            } else {
+                fileName = new String(fileName.getBytes(StandardCharsets.UTF_8), StandardCharsets.ISO_8859_1);
+            }
+            XWPFDocument doc = WordExportUtil.exportWord07(templatePath, params);
+            String tmpPath = temDir + fileName;
+            FileOutputStream fos = new FileOutputStream(tmpPath);
+            doc.write(fos);
+            // 设置强制下载不打开
+            response.setContentType("application/force-download");
+            // 设置文件名
+            response.addHeader("Content-Disposition", "attachment;fileName=" + fileName);
+            OutputStream out = response.getOutputStream();
+            doc.write(out);
+            out.close();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+//			delAllFile(temDir);//这一步看具体需求，要不要删
+        }
+
+    }
+
+    /**
+     * 用户自定义导出
+     *
+     * @param fileName 文件名
+     * @param response 响应
+     * @param workbook 文档
+     */
+    public static void userDefinedExport(String fileName, HttpServletResponse response, Workbook workbook) throws IOException {
+        downLoadExcel(fileName, response, workbook);
+    }
 
     /**
      * excel 导出
@@ -34,34 +154,6 @@ public class EasyPOIUtil {
      */
     public static void exportExcel(List<Map<String, Object>> list, String fileName, HttpServletResponse response) throws IOException {
         defaultExport(list, fileName, response);
-    }
-
-    /**
-     * 默认的 excel 导出
-     *
-     * @param list     数据列表
-     * @param fileName 导出时的excel名称
-     * @param response response
-     */
-    private static void defaultExport(List<Map<String, Object>> list, String fileName, HttpServletResponse response) throws IOException {
-        //把数据添加到excel表格中
-        Workbook workbook = ExcelExportUtil.exportExcel(list, ExcelType.HSSF);
-        downLoadExcel(fileName, response, workbook);
-    }
-
-    /**
-     * excel 导出
-     *
-     * @param list         数据列表
-     * @param pojoClass    pojo类型
-     * @param fileName     导出时的excel名称
-     * @param response     response
-     * @param exportParams 导出参数（标题、sheet名称、是否创建表头，表格类型）
-     */
-    private static void defaultExport(List<?> list, Class<?> pojoClass, String fileName, HttpServletResponse response, ExportParams exportParams) throws IOException {
-        //把数据添加到excel表格中
-        Workbook workbook = ExcelExportUtil.exportExcel(exportParams, pojoClass, list);
-        downLoadExcel(fileName, response, workbook);
     }
 
     /**
@@ -88,7 +180,9 @@ public class EasyPOIUtil {
      * @param response  response
      */
     public static void exportExcel(List<?> list, String title, String sheetName, Class<?> pojoClass, String fileName, HttpServletResponse response) throws IOException {
-        defaultExport(list, pojoClass, fileName, response, new ExportParams(title, sheetName, ExcelType.XSSF));
+        ExportParams exportParams = new ExportParams(title, sheetName, ExcelType.XSSF);
+        exportParams.setDictHandler(excelDictHandler);
+        defaultExport(list, pojoClass, fileName, response, exportParams);
     }
 
 
@@ -106,9 +200,42 @@ public class EasyPOIUtil {
     public static void exportExcel(List<?> list, String title, String sheetName, Class<?> pojoClass, String fileName, boolean isCreateHeader, HttpServletResponse response) throws IOException {
         ExportParams exportParams = new ExportParams(title, sheetName, ExcelType.XSSF);
         exportParams.setCreateHeadRows(isCreateHeader);
+        exportParams.setDictHandler(excelDictHandler);
         defaultExport(list, pojoClass, fileName, response, exportParams);
     }
 
+
+    /**
+     * 默认的 excel 导出
+     *
+     * @param list     数据列表
+     * @param fileName 导出时的excel名称
+     * @param response response
+     */
+    private static void defaultExport(List<Map<String, Object>> list, String fileName, HttpServletResponse response) throws IOException {
+        //把数据添加到excel表格中
+        Workbook workbook = ExcelExportUtil.exportExcel(list, ExcelType.HSSF);
+        downLoadExcel(fileName, response, workbook);
+    }
+
+    /**
+     * excel 导出
+     *
+     * @param list         数据列表
+     * @param pojoClass    pojo类型
+     * @param fileName     导出时的excel名称
+     * @param response     response
+     * @param exportParams 导出参数（标题、sheet名称、是否创建表头，表格类型）
+     */
+    private static void defaultExport(List<?> list, Class<?> pojoClass, String fileName, HttpServletResponse response, ExportParams exportParams) throws IOException {
+        //把数据添加到excel表格中
+        //Workbook workbook = ExcelExportUtil.exportExcel(exportParams, pojoClass, list);
+        // 导出大数据
+        IWriter<Workbook> writer = ExcelExportUtil.exportBigExcel(exportParams, pojoClass);
+        writer.write(list);
+        Workbook workbook = writer.get();
+        downLoadExcel(fileName, response, workbook);
+    }
 
     /**
      * excel下载
@@ -120,14 +247,63 @@ public class EasyPOIUtil {
     private static void downLoadExcel(String fileName, HttpServletResponse response, Workbook workbook) throws IOException {
         try {
             response.setCharacterEncoding("UTF-8");
-            response.setHeader("content-Type", "application/vnd.ms-excel");
-            response.setHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode(fileName + "." + ExcelTypeEnum.XLSX.getValue(), StandardCharsets.UTF_8.name()));
+            response.setContentType("application/octet-stream");
+            response.setCharacterEncoding("utf-8");
+            //response.setHeader("content-Type", "application/vnd.ms-excel");
+            response.setHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode(fileName + "." + ExcelTypeEnum.XLSX.getValue(), StandardCharsets.UTF_8));
             workbook.write(response.getOutputStream());
         } catch (Exception e) {
             throw new IOException(e.getMessage());
         }
     }
 
+    /**
+     * 读取单个单元格 可根据后缀名判断xls xlsx
+     *
+     * @param file      文件
+     * @param sheetName 表格
+     * @param rownum    行
+     * @param cellnum   列
+     * @return
+     * @throws Exception
+     */
+    public static Object readExcelData(MultipartFile file, String sheetName, int rownum, int cellnum) throws Exception {
+        String fileName = file.getOriginalFilename();
+        if (fileName.endsWith(ExcelTypeEnum.XLS.getValue())) {
+            HSSFWorkbook workbook = new HSSFWorkbook(file.getInputStream());
+            HSSFSheet sheet = workbook.getSheet(sheetName);
+            HSSFRow row = sheet.getRow(rownum);
+            HSSFCell cell = row.getCell(cellnum);
+            if (cell == null) {
+                return "";
+            }
+            switch (cell.getCellType()) {
+                case NUMERIC:
+                    return cell.getNumericCellValue();
+                case STRING:
+                    return cell.getStringCellValue();
+                default:
+                    return cell.getStringCellValue();
+            }
+        } else {
+            XSSFWorkbook workbook = new XSSFWorkbook(file.getInputStream());// 工作簿
+            XSSFSheet sheet = workbook.getSheet(sheetName);
+            ; // 工作表
+            XSSFRow row = sheet.getRow(rownum);
+            XSSFCell cell = row.getCell(cellnum);
+            if (cell == null) {
+                return "";
+            }
+            switch (cell.getCellType()) {
+                case NUMERIC:
+                    return cell.getNumericCellValue();
+                case STRING:
+                    return cell.getStringCellValue();
+                default:
+                    return cell.getStringCellValue();
+            }
+        }
+    }
 
     /**
      * excel 导入
