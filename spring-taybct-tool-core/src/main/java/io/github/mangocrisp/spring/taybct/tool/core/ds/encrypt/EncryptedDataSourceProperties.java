e652d23f1c171eeff5e1c91033e80b96cc277356
Mango Crisp <15014633363@163.com>
2025-05-30 16:34:34 +0800

    - 注释描述错误

--- spring-taybct-tool-core/src/main/java/io/github/mangocrisp/spring/taybct/tool/core/ds/encrypt/EncryptedDataSourceProperties.java
+++ spring-taybct-tool-core/src/main/java/io/github/mangocrisp/spring/taybct/tool/core/ds/encrypt/EncryptedDataSourceProperties.java
@@ -24,9 +24,9 @@ public class EncryptedDataSourceProperties {
      */
     private Class<? extends Function<String, String>> decryptFunction = SM4Coder.De.class;
     /**
-     * 解密方法，为每个数据源 指定一个 {@code Function<String, String>} 类来实现解密,怎么解密要看具体是怎么加密的，例如，sm2 加密的就用 {@code SM2EncryptedPassable}
+     * 解密方法，为每个数据源 指定一个 {@code Function<String, String>} 类来实现解密,怎么解密要看具体是怎么加密的，例如，sm4 加密的就用 {@code SM4Coder.En}
      *
-     * @see io.github.mangocrisp.spring.taybct.tool.sm2.config.SM2EncryptedPassable
+     * @see io.github.mangocrisp.spring.taybct.tool.core.util.sm.SM4Coder.En
      */
     private Map<String, Class<? extends Function<String, String>>> decryptFunctions = new HashMap<>();
     /**
